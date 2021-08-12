83512968c27d41be6b8a32d78e36cc570e64d2fa
sukumarpradhan <sukumarp0010@email.com>
2021-08-12 19:58:36 +0530

    33rd commit - new git branch dev

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Its New Edit <code>src/App.js</code> and save to reload.
+          Its New from Dev Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
